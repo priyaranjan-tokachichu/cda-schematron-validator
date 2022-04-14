77d6148ebda059f12926418877c8c62aba288733
Priyaranjan Tokachichu <priyaranjan@priyaranjan.com>
2022-04-14 04:16:47 -0400

    Separate sync and async code

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,6 @@ module.exports = function(grunt) {
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.registerTask('default', ['jshint', 'mochaTest']);
+    grunt.registerTask('default', ['mochaTest']);
     grunt.registerTask('api', ['mochaTest']);
 };
